3d6012f49e05a2d6c67c9b8ea9f3f8b4bcf62a34
Alexis <dnc.alexis@gmail.com>
2025-02-09 16:53:14 +0100

    improvment: add active state (menu) + optimisations + docker build config

--- app/src/components/Header/Menu.tsx
+++ app/src/components/Header/Menu.tsx
@@ -1,16 +1,22 @@
 import './Menu.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Menu() {
+  const location = useLocation();
+  const isActivePage = (path: string) => {
+    const currentPath = location.pathname;
+    return currentPath === path ? 'active' : '';
+  };
+
   return (
     <ul className='menu-container'>
       <li>
-        <Link className='menu-link' to='/' data-testid='home'>
+        <Link className={`menu-link ${isActivePage('/')}`} to='/' data-testid='home'>
           Accueil
         </Link>
       </li>
       <li>
-        <Link className='menu-link' to='/informations' data-testid='about'>
+        <Link className={`menu-link ${isActivePage('/informations')}`} to='/informations' data-testid='about'>
           Informations
         </Link>
       </li>
