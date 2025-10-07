3b5757d0d50682a9ec3695ef61bc0900b454f185
N-Reptyl <nassim.mourabit@outlook.fr>
2025-10-07 17:01:29 +0200

    feat(Home): add days left until birthday

---
+++ src/utils/date.ts
@@ -0,0 +1,10 @@
+export function daysLeft(birthDate: string): number {
+    const today = new Date();
+    const birth = new Date(birthDate);
+    birth.setFullYear(today.getFullYear());
+
+    const diff = birth.getTime() - today.getTime();
+    const days = Math.ceil(diff / (1000 * 3600 * 24));
+
+    return days >= 0 ? days : 0;
+}
