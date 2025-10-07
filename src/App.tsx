8aab3576d360a6f661d53080583589a26109e7b7
N-Reptyl <nassim.mourabit@outlook.fr>
2025-10-07 13:39:00 +0200

    feat: setup react ts + pages and routing

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Informations from './pages/Informations';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/" className="nav-link">Accueil</Link>
+        <Link to="/informations" className="nav-link">Informations</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/informations" element={<Informations />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
