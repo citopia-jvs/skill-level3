76e54f533d3e077f6a8ec2034c6982c60a7bfd9e
N-Reptyl <nassim.mourabit@outlook.fr>
2025-10-07 16:49:15 +0200

    feat(Home): show dummyJSON image based on user info

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
+import { Home } from './pages/Home';
 import { Informations } from './pages/Informations';
 
 export default function App() {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,27 @@
-import { Component } from 'react'
+import { useUserStore } from "../store/userStore";
+import React, { useEffect, useState } from "react";
 
-export default class Home extends Component {
-  render() {
+export const Home: React.FC = () => {
+    const { name, firstName } = useUserStore(); // User info from store
+    const [imgDummy, setImgDummy] = useState<string | null>(null); // Image src from dummyjson
+
+    // Fetch the image from dummyjson
+    useEffect(() => {
+        if (!name || !firstName) return;
+        const encodedName = encodeURIComponent(`Bonjour ${firstName} ${name} !`);
+        const url = `https://dummyjson.com/image/600x200/FF426D/ffffff?text=${encodedName}`;
+
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    setImgDummy(response.url);
+                }
+            });
+    }, [firstName, name]);
+
     return (
-      <div>Home</div>
-    )
-  }
+    <div>
+        {imgDummy ? <img src={imgDummy} alt="User Dummy" /> : <p> Veuillez renseigner vos informations.</p>}
+    </div>
+    );
 }
