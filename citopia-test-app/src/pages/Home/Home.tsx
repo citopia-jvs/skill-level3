374f36f6a9e13992d9c0d05ff4d947a29e42dbd6
Calas <barbara.calas@orange.fr>
2025-10-09 11:29:58 +0200

    feat(weather): Bonus feature: Add weather display on Home page. Integrate temperature and weather description.

--- citopia-test-app/src/pages/Home/Home.tsx
+++ citopia-test-app/src/pages/Home/Home.tsx
@@ -1,18 +1,21 @@
-import React, { useContext, useMemo, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import PageTitle from "../../components/PageTitle/PageTitle";
 import { UserContext } from "../../context/UserContext";
 import { calculateDaysToBirthday } from "../../utils/dateUtils";
 import './Home.css';
 import NavigationLink from "../../components/NavigationLink/NavigationLink";
+import { fetchWeather, getWeatherDescription } from "../../utils/weatherUtils";
 
 /**
  * Page d'accueil:
  * - Affiche une image DummyJSON dynamique avec le prénom et le nom de l'utilisateur.
  * - Affiche le nombre de jours restants avant le prochain anniversaire de l'utilisateur.
  * - Gère les cas où les informations de l'utilisateur ne sont pas encore définies.
+ * - Bonus: affichage de la météo à Citopia (Saint-Martin sur le Pré)
  */
 const Home: React.FC = () => {
     const [isImageLoaded, setIsImageLoaded] = useState(false);
+    const [weather, setWeather] = useState<{ temperature: number; weathercode: number} | null>(null);
 
     const userContext = useContext(UserContext);
     const user = userContext?.user;
@@ -32,6 +35,20 @@ const Home: React.FC = () => {
         const birthDate = user?.birthDate ? new Date(user.birthDate) : null;
         return calculateDaysToBirthday(birthDate)
     }, [user?.birthDate]);
+
+    useEffect(() => {
+        async function getWeather() {
+            try {
+                const latitudeCitopia = 48.971431;
+                const longitudeCitopia = 4.345522;
+                const data = await fetchWeather(latitudeCitopia, longitudeCitopia);
+                setWeather(data);
+            } catch (error) {
+                console.error(error);
+            }  
+        }
+        getWeather();
+    }, []);
     
     if(!userContext) {
         return <div className="message-erreur">Erreur: contexte utilisateur non trouvé</div>
@@ -41,6 +58,14 @@ const Home: React.FC = () => {
         <>
             <div className="home-container">
                 <PageTitle title="Accueil" className="page-title"/>
+
+                <div>
+                    {weather && (
+                        <p>
+                            {getWeatherDescription(weather.weathercode)} - {weather.temperature}°C à Citopia
+                        </p>
+                    )}
+                </div>
 
                 <div>
                     {user?.lastName && user?.firstName && dummyImageUrl ? (
